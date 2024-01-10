6e8e277625c59f089080b2c6d43509ade759f27f
JulSarmiento <jasa1999@hotmail.com>
2024-01-10 16:01:47 -0500

    Botones, textos, imagenes e iconos

--- app/src/main/java/com/studycorn/udemychallenge/MainActivity.kt
+++ app/src/main/java/com/studycorn/udemychallenge/MainActivity.kt
@@ -3,38 +3,23 @@ package com.studycorn.udemychallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.studycorn.udemychallenge.ui.theme.UdemychallengeTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,22 +40,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyTextFieldFather() {
-    var myText by rememberSaveable { mutableStateOf("Hello") }
-    MyTextField(myText) { myText = it }
+fun MyImage() {
+    // general image
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background), // ImageBitmap
+        contentDescription = "My Image", // for accessibility
+        alpha = 0.5f, // opacity
+    )
 }
 
 @Composable
-fun MyButtonExample() {
-    Column {
-
-    }
+fun MyImageAdvanced() {
+    // Advance image
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background), // ImageBitmap
+        contentDescription = "My Image", // for accessibility
+        alpha = 0.5f, // opacity
+        modifier = Modifier
+            .clip(CircleShape)
+            .border(5.dp, Color.Blue)
+    )
 }
 
+@Composable
+fun MyIcon() {
+    Icon(
+        imageVector = Icons.Rounded.Star,
+        contentDescription = "My Icon",
+    )
 
 
+}
 
-@Preview(showBackground = true, name = "Default Preview")
+@Preview(showBackground = true, name = "Default Preview", showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     UdemychallengeTheme {
@@ -90,8 +92,10 @@ fun GreetingPreview() {
 //        MyTextFieldAdvance()
 //        MyTextFieldOutlined()
 //        fun MyTextFieldFather()
-        MyButtonExample()
-
+//        MyButtonExample()
+//        MyImage()
+//        MyImageAdvanced()
+        MyIcon()
     }
 }
 
---
+++ app/src/main/java/com/studycorn/udemychallenge/MyButtons.kt
@@ -0,0 +1,89 @@
+package com.studycorn.udemychallenge
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+fun MyButtonExample() {
+    var enabled by rememberSaveable {
+        mutableStateOf(true)
+    }
+
+    ConstraintLayout (
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        val (button1, button2, button3) = createRefs()
+
+        Button(
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            onClick = {enabled = false},
+            enabled = enabled,
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Red
+            ),
+            border = BorderStroke(2.dp, Color.Black),
+        ) {
+            Text(text = "Normal Button")
+        }
+
+        OutlinedButton(
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(button1.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            onClick = {enabled = false},
+            enabled = enabled,
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Red,
+                disabledContainerColor = Color.Gray
+            ),
+            border = BorderStroke(2.dp, Color.Black),
+        ) {
+            Text(text = "Outlined Button")
+        }
+
+        TextButton(
+            modifier = Modifier.constrainAs(button3) {
+                top.linkTo(button2.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            },
+            onClick = {enabled = false},
+            enabled = enabled,
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Red
+            ),
+            border = BorderStroke(2.dp, Color.Black),
+        ) {
+            Text(text = "Text Button")
+        }
+
+        createVerticalChain(button1, button2, button3, chainStyle = ChainStyle.Spread)
+    }
+}
